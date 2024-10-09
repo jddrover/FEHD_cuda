8f79c48e74bbb6c444e06700117cb2d8f08cdb03
jddrvr <40179259+jddrvr@users.noreply.github.com>
2024-10-09 09:55:09 -0400

    Update FEHD.h

--- FEHD.h
+++ FEHD.h
@@ -3,9 +3,9 @@
 #include "dataContainers.h"
 #include "utility.h"
 // Computes the gradient for multiple rotations of an AR model.
-void compGradient(ARmodel, std::vector<float> &, std::vector<float>,std::vector<float>,paramContainer,int,std::vector<int>);
+//void compGradient(ARmodel, std::vector<float> &, std::vector<float>,std::vector<float>,paramContainer,int,std::vector<int>);
 // The main call of the FEHD routine
-//void runFEHD(dataList, std::vector<float> &, paramContainer);
+void runFEHD(dataList, std::vector<float> &, paramContainer);
 // Executes the minimization of the upward Granger causality.
 //void runFEHDstep(std::vector<float> &, matrix &, dataList, paramContainer, int);
 // Computes a single rotation matrix from a list of angles. 
