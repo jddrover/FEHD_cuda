104a3512b53a08c4ced100feb6eed802d28c064f
Jonathan Drover <jddrvr@gmail.com>
2025-06-03 17:40:10 -0400

    done for the day

--- PGC_vcpp.cpp
+++ PGC_vcpp.cpp
@@ -3,6 +3,8 @@
 #include <random>
 #include <vector>
 #include <iostream>
+#include <complex>
+//#include <cmath>
 #include <algorithm>
 #include "utility.h"
 
@@ -12,6 +14,9 @@ void PGC(std::vector<float> dataArray, paramContainer params)
   // Size of large arrays 
   int numEpochs = params.numEpochs;
   int numComps = params.numChannels;
+  std::vector<float> freq(params.numFreqs);
+  for(int findx=0;findx<params.numFreqs;findx++)
+    freq[findx] = (params.fMax-params.fMin)/(params.numFreqs-1)*float(findx)+params.fMin;
   // Check that this is an integer
   int epochPts = params.epochPts;
   std::vector<int> lagList(params.lagList);
@@ -39,8 +44,14 @@ void PGC(std::vector<float> dataArray, paramContainer params)
     
   std::vector<float> LS(2*numLags*numEpochs*(epochPts-maxLag),0.0);
   std::vector<float> RS(2*numEpochs*(epochPts-maxLag),0.0);
+  std::vector<float> RS2(2*numEpochs*(epochPts-maxLag),0.0);
   std::vector<float> LCOV(2*numLags*2*numLags,0.0);
   std::vector<float> RCOV(2*numLags*2,0.0);
+  std::vector<float> A(4*numLags,0.0);
+  std::vector<float> resCOV(4,0.0);
+  std::vector<float> Pmat(4,0.0);
+  std::vector<float> Pinv(4,0.0);
+  
   int info;
   int IPIV[2*numLags];
   const float alpha=1.0;
@@ -58,19 +69,62 @@ void PGC(std::vector<float> dataArray, paramContainer params)
 
 	cblas_ssyrk(CblasColMajor,CblasUpper,CblasNoTrans,2*numLags,numEpochs*(epochPts-maxLag),
 		    alpha,LS.data(),2*numLags,0.0,LCOV.data(),2*numLags);
-	
-	// I only have the upper triangular part of the covariance matrix. 
-	// This should be ok.
 
 	cblas_sgemm(CblasColMajor,CblasNoTrans,CblasTrans,2*numLags,2,numEpochs*(epochPts-maxLag),
 		    alpha,LS.data(),2*numLags,RS.data(),2,0.0,RCOV.data(),2*numLags);
-		    
 	
 	info = LAPACKE_ssysv(LAPACK_COL_MAJOR,'U',2*numLags,2,LCOV.data(),2*numLags,IPIV,
 			     RCOV.data(),2*numLags);
 	
+	for(int col=0;col<numLags*2;col++)
+	  {
+	    cblas_scopy(2,RCOV.data()+col,2*numLags,A.data()+col*2,1);
+	  }
+
+	// Compute the residuals
+	// RHS-A*LHS
+
+	cblas_sgemm(CblasColMajor,CblasNoTrans,CblasNoTrans,2,numEpochs*(epochPts-maxLag),2*numLags,
+		    -1.0,A.data(),2,LS.data(),2*numLags,1.0,RS.data(),2);
+
+	cblas_sgemm(CblasColMajor,CblasNoTrans,CblasTrans,2,2,numEpochs*(epochPts-maxLag),
+		    1.0,RS.data(),2,RS.data(),2,0.0,resCOV.data(),2);
+	
+      
+
+	Pmat[0]=1.0;
+	Pmat[1]=-resCOV[2]/resCOV[0];
+	Pmat[2]=0.0;
+	Pmat[3]=1.0;
+	
+	Pinv[0]=1.0;
+	Pinv[1]=-Pmat[1];
+	Pinv[2]=0.0;
+	Pinv[3]=Pinv[3];
+
+	cblas_sgemm(CblasColMajor,CblasNoTrans,CblasNoTrans,2,2*numLags,2,
+		    1.0,Pmat.data(),2,A.data(),2,0.0,RCOV.data(),2);
+	for(int lag=0;lag<numLags;lag++)
+	  {
+	    cblas_sgemm(CblasColMajor,CblasNoTrans,CblasNoTrans,2,2,2,
+			1.0,RCOV.data()+lag*4,2,Pinv.data(),2,0.0,A.data()+lag*4,2);
+	  }
+
+	// Apply PR
+	cblas_sgemm(CblasColMajor,CblasNoTrans,CblasNoTrans,2,numEpochs*(epochPts-maxLag),2,
+		    1.0,Pmat.data(),2,RS.data(),2,0.0,RS2.data(),2);
+	// Compute covariance.
+	cblas_sgemm(CblasColMajor,CblasNoTrans,CblasTrans,2,2,numEpochs*(epochPts-maxLag),
+		    1.0,RS2.data(),2,RS2.data(),2,0.0,resCOV.data(),2); // Reusing resCOV
+
+
+	for(int findx=0;findx<params.numFreqs;findx++)
+	  {
+	    for(int lag=0;lag<params.numLags;lag++)
+	      {
+		argmt = std::exp(std::complex<float>(0.0,argmtBASE*(float)lagList[lag]*freq[findex]));
+	
       }
-  
   return;
   }
 
